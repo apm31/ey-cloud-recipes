745bc6c35e79d81ab724d62f11ef2aca262f4fe1
Brenda Egeland <begeland@allastaparty.com>
2015-07-10 16:31:11 -0400

    Add comment

--- cookbooks/wkhtmltopdf/recipes/default.rb
+++ cookbooks/wkhtmltopdf/recipes/default.rb
@@ -4,6 +4,8 @@
 #
 # See support ticket https://support.cloud.engineyard.com/hc/en-us/requests/98142
 
+# Chef how-to: https://support.cloud.engineyard.com/hc/en-us/articles/205407438-Customize-Your-Environment-with-Chef-Recipes
+
 enable_package "media-gfx/wkhtmltopdf-bin" do
   version "0.12.2.1"
 end

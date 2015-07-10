d1921781f60814c14a1b2afd9faa1365ffba8ef2
Brenda Egeland <begeland@allastaparty.com>
2015-07-10 16:13:27 -0400

    Adding wkhtmltopdf

--- cookbooks/main/recipes/default.rb
+++ cookbooks/main/recipes/default.rb
@@ -4,6 +4,8 @@
 #  }
 #end
 
+inlucde_recipe "wkhtmltopdf"
+
 # uncomment to deny access to /log, /config, and .git directories as well as any .yml files
 # include_recipe "deny-directories"
 
---
+++ cookbooks/wkhtmltopdf/recipes/default.rb
@@ -0,0 +1,14 @@
+#
+# Cookbook Name:: wkhtmltopdf
+# Recipe:: default
+#
+# See support ticket https://support.cloud.engineyard.com/hc/en-us/requests/98142
+
+enable_package "media-gfx/wkhtmltopdf-bin" do
+  version "0.12.2.1"
+end
+
+package "media-gfx/wkhtmltopdf-bin" do
+  version "0.12.2.1"
+  action :install
+end
